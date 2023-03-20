ff1eaaf09055c361182e250f17e817072fb6742d
Jenniferarokiadoss <128289296+Jenniferarokiadoss@users.noreply.github.com>
2023-03-20 20:28:46 +1100

    Add files via upload

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 
 
-//process.cwd() method is used in Node.js process's active working directory .
-
-//Rerfernce from https://www.youtube.com/watch?v=ObikkECrapg
-//https://www.geeksforgeeks.org/node-js-process-cwd-method/
+//__dirname an environment variable that displays 
+//the full path to the directory where the active file is located. .
+//Rerfernce from https://www.geeksforgeeks.org/difference-between-__dirname-and-in-node-js/
+//https://www.youtube.com/watch?v=l8wooO1nh70
 app.get('/', (req, res) => {
   res.sendFile( __dirname +  '/index.html');
 });
 
-app.listen(3030, () => {
-  console.log('Server listening on port 3030');
-});
+//list to a particular port
+app.listen(3000)
