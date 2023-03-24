dac14c00889847d5b70f6c933189e1aa575c5054
Jenniferarokiadoss <128289296+Jenniferarokiadoss@users.noreply.github.com>
2023-03-24 23:36:00 +1100

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var express = require('express')
-var app = express()
 
-//__dirname an environment variable that displays 
-//the full path to the directory where the active file is located. .
-//Rerfernce from https://www.geeksforgeeks.org/difference-between-__dirname-and-in-node-js/
-//https://www.youtube.com/watch?v=l8wooO1nh70
+
+
+// //__dirname an environment variable that displays 
+// //the full path to the directory where the active file is located. .
+// //Rerfernce from https://www.geeksforgeeks.org/difference-between-__dirname-and-in-node-js/
+// //https://www.youtube.com/watch?v=l8wooO1nh70
+
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
-  res.sendFile( __dirname +  '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-//list to a particular port
-app.listen(3000)
+app.listen(3030, () => {
+  console.log('Server listening on port 3030');
+});
